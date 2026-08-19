f91da0274c702e447724e2d9af31b37052cc1e1e
Legend <therealragnartv@gmail.com>
2026-08-19 09:18:38 -0500

    Clean unused NPC portrait assets

--- npc-art.js
+++ npc-art.js
@@ -1,11 +1,5 @@
 window.PORTALDB_NPC_ART = {
   arlette: "assets/npcs/arlette.webp?v=20260819-master-1",
-  breda: "assets/npcs/breda.webp?v=20260818-3",
-  eren: "assets/npcs/eren.webp?v=20260818-3",
-  hogan: "assets/npcs/hogan.webp?v=20260818-3",
-  leaf: "assets/npcs/leaf.webp?v=20260818-3",
-  neria: "assets/npcs/neria.webp?v=20260818-3",
-  rendall: "assets/npcs/rendall.webp?v=20260818-3",
-  theron: "assets/npcs/theron.webp?v=20260818-3"
+  breda: "assets/npcs/breda.webp?v=20260818-3"
 };
 
