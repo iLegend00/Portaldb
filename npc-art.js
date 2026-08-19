19ba1527b147226ef01fe96bc72c3e3d365b445c
Legend <therealragnartv@gmail.com>
2026-08-18 21:11:09 -0500

    Finalize NPC art asset map

--- npc-art.js
+++ npc-art.js
@@ -1,1 +1,10 @@
-window.PORTALDB_NPC_ART = window.PORTALDB_NPC_ART || {};
+window.PORTALDB_NPC_ART = {
+  arlette: "assets/npcs/arlette.webp",
+  breda: "assets/npcs/breda.webp",
+  eren: "assets/npcs/eren.webp",
+  hogan: "assets/npcs/hogan.webp",
+  leaf: "assets/npcs/leaf.webp",
+  neria: "assets/npcs/neria.webp",
+  rendall: "assets/npcs/rendall.webp",
+  theron: "assets/npcs/theron.webp"
+};
