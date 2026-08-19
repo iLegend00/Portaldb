25d70dcb16ddfcefa166c5bb5a101b5f0e5a86fc
Legend <therealragnartv@gmail.com>
2026-08-19 09:20:46 -0500

    Refresh cleaned NPC portrait registry

--- app.js
+++ app.js
@@ -3,5 +3,6 @@
   css.rel='stylesheet';
   css.href='npc-art.css';
   document.head.appendChild(css);
-  document.write('<script src="npc-art.js"><\/script><script src="app-core.js"><\/script><script src="npc-art-ui.js"><\/script>');
+  document.write('<script src="npc-art.js?v=20260819-clean-slate-1"><\/script><script src="app-core.js"><\/script><script src="npc-art-ui.js"><\/script>');
 })();
+
