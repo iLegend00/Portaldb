bfbb44eee201ffa25bb784e6b22b3c15f890d4e3
Legend <therealragnartv@gmail.com>
2026-08-19 09:13:28 -0500

    Fix Arlette portrait asset

--- npc-art.js
+++ npc-art.js
@@ -1,5 +1,5 @@
 window.PORTALDB_NPC_ART = {
-  arlette: "assets/npcs/arlette.webp?v=20260819-arlette-clean-2",
+  arlette: "assets/npcs/arlette.webp?v=20260819-master-1",
   breda: "assets/npcs/breda.webp?v=20260818-3",
   eren: "assets/npcs/eren.webp?v=20260818-3",
   hogan: "assets/npcs/hogan.webp?v=20260818-3",
@@ -8,3 +8,4 @@ window.PORTALDB_NPC_ART = {
   rendall: "assets/npcs/rendall.webp?v=20260818-3",
   theron: "assets/npcs/theron.webp?v=20260818-3"
 };
+
