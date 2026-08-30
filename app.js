e2036206c97c01b9ae3a157601e2a7c88b7030c5
Legend <therealragnartv@gmail.com>
2026-08-30 14:19:28 -0500

    Target NPC service layout explicitly

--- app.js
+++ app.js
@@ -5,12 +5,12 @@
   document.head.appendChild(css);
   const npcProfileCss=document.createElement('link');
   npcProfileCss.rel='stylesheet';
-  npcProfileCss.href='npc-profile.css?v=20260830-npc-profile-3';
+  npcProfileCss.href='npc-profile.css?v=20260830-npc-profile-4';
   document.head.appendChild(npcProfileCss);
   const verificationCss=document.createElement('link');
   verificationCss.rel='stylesheet';
   verificationCss.href='verification.css?v=20260828-two-state-provenance-1';
   document.head.appendChild(verificationCss);
-  document.write('<script src="verification.js?v=20260828-two-state-provenance-1"><\/script><script src="npc-art.js?v=20260819-all-npcs-1"><\/script><script src="app-core.js?v=20260830-npc-profile-3"><\/script>');
+  document.write('<script src="verification.js?v=20260828-two-state-provenance-1"><\/script><script src="npc-art.js?v=20260819-all-npcs-1"><\/script><script src="app-core.js?v=20260830-npc-profile-4"><\/script>');
 })();
 
