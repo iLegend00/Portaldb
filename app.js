175108fc463ef5cab10b0b1edbb53bbae1cbbed6
Legend <therealragnartv@gmail.com>
2026-09-11 12:36:52 -0500

    Lock internal pages to solid parchment palette

--- app.js
+++ app.js
@@ -5,15 +5,15 @@
   document.head.appendChild(css);
   const npcProfileCss=document.createElement('link');
   npcProfileCss.rel='stylesheet';
-  npcProfileCss.href='npc-profile.css?v=20260911-solid-parchment-1';
+  npcProfileCss.href='npc-profile.css?v=20260911-solid-parchment-2';
   document.head.appendChild(npcProfileCss);
   const recordProfileCss=document.createElement('link');
   recordProfileCss.rel='stylesheet';
-  recordProfileCss.href='record-profile.css?v=20260911-solid-parchment-1';
+  recordProfileCss.href='record-profile.css?v=20260911-solid-parchment-2';
   document.head.appendChild(recordProfileCss);
   const equipmentProfileCss=document.createElement('link');
   equipmentProfileCss.rel='stylesheet';
-  equipmentProfileCss.href='equipment-profile.css?v=20260911-solid-parchment-1';
+  equipmentProfileCss.href='equipment-profile.css?v=20260911-solid-parchment-2';
   document.head.appendChild(equipmentProfileCss);
   const verificationCss=document.createElement('link');
   verificationCss.rel='stylesheet';
